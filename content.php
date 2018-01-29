3bbcb9a8e71a6504ccc41f64ab86d2bf47af17c0
YowYow022 <35923651+YowYow022@users.noreply.github.com>
2018-01-29 14:29:25 +0100

    Add files via upload

---
+++ content.php
@@ -0,0 +1,73 @@
+<div id="content">
+	<div>
+		<img id="image1" src="images\123.jpg" style="float:left; margin-left: -40px;" />
+	</div>
+	<div id="form2">
+		<form action="" method="post">
+			<h2>Sign up today</h2>
+			<table>
+				<tr>
+					<td align="right"><strong>Login:</strong></td>
+					<td><input type="text" name="u_login" required="required" placeholder="Login"/></td>
+				</tr>
+				<tr>
+					<td align="right"><strong>Mot de passe:</strong></td>
+					<td><input type="password" name="u_mdp" required="required" placeholder="*****"/></td>
+				</tr>
+				<tr>
+					<td align="right"><strong>Nom:</strong></td>
+					<td><input type="text" name="u_nom" required="required" placeholder="Nom"/></td>
+				</tr>
+				<tr>
+					<td align="right"><strong>Prénom:</strong></td>
+					<td><input type="text" name="u_prenom" required="required" placeholder="Prénom"/></td>
+				</tr>
+				<tr>
+					<td align="right"><strong>Date de naissance:</strong></td>
+					<td><input type="date" name="u_naissance" required="required" placeholder="dd/mm/yyyy" /></td>
+				</tr>
+				<tr>
+					<td align="right"><strong>Genre:</strong></td>
+					<td>
+						<select name="u_genre">
+							<option>Genre ?</option>
+							<option>Femme</option>
+							<option>Homme</option>
+						</select>
+					</td>
+				</tr>
+				<tr>
+					<td align="right"><strong>E-mail:</strong></td>
+					<td><input type="email" name="u_email" required="required" placeholder="*****@enssat.fr"/></td>
+				</tr>
+				<tr>
+					<td align="right"><strong>Année:</strong></td>
+					<td><input type="text" name="u_annee" required="required" placeholder="yyyy"/></td>
+				</tr>
+				<tr>
+					<td align="right"><strong>Discipline:</strong></td>
+					<td>								
+						<select name="u_discipline">
+							<option>Discipline ?</option>
+							<option>IMR</option>
+							<option>PHO</option>
+							<option>INF</option>
+							<option>ELE</option>
+						</select></td>
+				</tr>
+						
+				<tr>
+					<td></td>
+					<td>
+						<button id="but" name="Valider">Valider</button>
+					</td>
+				</tr>
+			</table>
+		</form>
+	</div>
+</div>
+
+
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" lang="fr" />
+		<title>Réseau Social</title>
+		<link rel="stylesheet" href="styles/style.css" />
+	</head>
+	
+	
+<body>
+	<div class="container">
+		<div id="header_wrapper">
+			<div id="header">
+				<img id="image" src="images\logo_enssat.jpg" style="float:left;" />
+				<form method="post" action="" id="form1">
+					<strong>Email:</strong>
+					<input type="email" name="email" placeholder="Email" required="required" />
+					<strong>Password:</strong>
+					<input type="password" name="pass" placeholder="*****" required="required" />
+					<button name="login"> Login </button>
+				</form>
+			</div>
+		</div>
+	</div>
