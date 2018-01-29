20800958f4e35689b75ed5611d458647791d18a3
NicolasHEnssat <35795421+NicolasHEnssat@users.noreply.github.com>
2018-01-29 14:37:52 +0100

    Add files via upload
    Squelette html page perso, plus dans le futur.
    Images uploadées en zip à la fin.

---
+++ Base_page.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
+
+<head>
+	<meta charset="utf-8" />
+	<title>ENSSATBook</title>
+	<meta name="description" content="" />
+	<meta name="keywords" content="social, network, ENSSAT" />
+    <link rel="stylesheet" type="text/css" href="style-accueil.css" />
+</head>
+
+<body>
+	<header>
+
+
+		<!-- Barre de recherche -->
+		<div class="searchbar">
+		  <a class="searchItems" href="#home">Home</a>
+			<img src="img_reseau-soc/parameter-icon.png" href="#home" id="parameterico">
+		  <a class="searchItems" href="#amis">Amis</a>
+		  <input type="text" placeholder="Search...">
+			<input type="submit" value="Rechercher">
+		</div>
+
+	</header>
+
+<!-- Un bloc amis/groupes/evennements -->
+
+<section class="content">
+	<div class="global_content">
+		<p>Le fil d'actualités ici</p>
+	</div>
+	<div class="lalala" id="Event">
+		<p>Even't</p>
+	</div>
+	<div class="lalala" id="groupes">
+		<p>groupes</p>
+	</div>
+
+	<div class="none" id="chat">
+		<p>Amis</p>
+	</div>
+
+</section>
+
+
+
+</body>
