331df8bf155a2c8f4a581018b3f82fa5a56810cf
NicolasHEnssat <35795421+NicolasHEnssat@users.noreply.github.com>
2018-02-14 13:50:54 +0100

    Add files via upload

--- squelette.php
+++ squelette.php
@@ -1,9 +1,6 @@
 <?php
-
-class page{
-
-
-
+class page
+{
   protected $page;
 
   public function __construct()
@@ -18,11 +15,24 @@ class page{
       }
       return false;
   }
+  public function add_content($content)
+  {
+      if(is_scalar($content)) {
+        $this->page['content'][] = $content;
+        return true;
+      }
+      return false;
+  }
 
   public function execute()
   {
     $this->header();
     $this->body();
+    if(is_array($this->page['body']))
+    {
+      echo implode("\n", $this->page['body']);
+    }
+    $this->footer();
   }
 
 
@@ -33,10 +43,16 @@ class page{
       <html lang="fr">
       <head>
           <meta charset="utf-8">
-          <title>TP2 PHP</title>
+          <title>ENSSATBOOK</title>
           <meta name="description" content="Projet réseau social">
           <meta name="author" content="MMNY">
+          <meta name="keywords" content="social, network, ENSSAT" />
+            <link rel="stylesheet" type="text/css" href="style-accueil.css" />
       </head>
+      <style>
+        background-color: <?php echo $u_colorfond; ?>;
+        color : <?php echo $u_colortext; ?>;
+      </style>
       <body>
       ';
   }
@@ -45,14 +61,19 @@ class page{
   {
     echo '
       <div class="searchbar">
-        <a href="Base_page.php"><img src="img_reseau-soc/simple-home.png" class="parameterico"></a>
+        <a href="Home.php"><img src="img_reseau-soc/simple-home.png" class="parameterico"></a>
         <a href="#parametres"><img src="img_reseau-soc/parameter-icon.png" class="parameterico"></a>
         <a href="#chat"><img src="img_reseau-soc/chat-bubble.png" class="parameterico"></a>
         <input type="text" placeholder="Search...">
         <input type="submit" value="Rechercher">
       </div>
-      '
+      ';
   }
+
+  protected function content()
+  {
+  }
+
   protected function footer()
   {
       echo '
